e86719a3a3a9b71b4f0e1c2cd8eabd828e854744
Juan David Muñoz Espinosa <jdmuoesp@gmail.com>
2026-05-05 16:11:12 +0200

    jdmuoesp:Implementación del modo ligth o dark

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,12 +4,17 @@ class AppTheme {
 
   final Color electusColor;
 
+  final bool tenebristModusEts;
+
   AppTheme ({
-    this.electusColor = const Color(0xFF1E1C36)
+    this.electusColor = const Color(0xFF1E1C36),
+    this.tenebristModusEts = false
   });
 
   ThemeData getTheme() => ThemeData(
     colorSchemeSeed: electusColor,
+
+    brightness: tenebristModusEts ? Brightness.dark : Brightness.light,
 
     appBarTheme: AppBarTheme(
 
